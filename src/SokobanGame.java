3a460085af995685b82041be426ac959d5c591d5
Phi Hung <91715559+gakonlonton@users.noreply.github.com>
2022-11-22 01:19:33 +0700

    Last update

--- src/SokobanGame.java
+++ src/SokobanGame.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
@@ -17,9 +18,10 @@ public class SokobanGame extends Application {
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/GameMaster.fxml")));
         Scene scene = new Scene(root);
-        scene.setFill(Paint.valueOf("#383838"));
+        scene.setFill(Paint.valueOf("#2A2A2A"));
 
         stage.setTitle("Sokoban");
+        stage.getIcons().add(new Image("textures/crate.gif"));
         stage.setScene(scene);
         stage.show();
     }
--- src/controller/Board.java
+++ src/controller/Board.java
@@ -105,6 +105,6 @@ public class Board {
     }
 
     public boolean completed() {
-        return target == numberOfCrate;
+        return (target == numberOfCrate);
     }
 }
